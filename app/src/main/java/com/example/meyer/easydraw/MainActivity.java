6890cc9c31a2bbdc73fe53c660ed3d7098da5ab7
Dugesia <meyerstefanms@outlook.com>
2018-01-09 16:52:21 +0100

    grob fertig

--- app/src/main/java/com/example/meyer/easydraw/MainActivity.java
+++ app/src/main/java/com/example/meyer/easydraw/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.meyer.easydraw;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnTouchListener {
+
+    ImageView imageView;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+    int BrushSize=20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +39,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +48,21 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        imageView = (ImageView)  findViewById(R.id.main_imageView);
+        Display currentDisplay = getWindowManager().getDefaultDisplay();
+        float dw = currentDisplay.getWidth();
+        float dh = currentDisplay.getHeight();
+
+
+
+        bitmap = Bitmap.createBitmap((int) dw, (int) dh, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        imageView.setImageBitmap(bitmap);
+        imageView.setOnTouchListener(this);
     }
 
     @Override
@@ -54,48 +77,85 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            ClearImage();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    public void ClearImage(){
+        paint.setColor(Color.WHITE);
+        canvas.drawRect(0,0,canvas.getWidth(),canvas.getHeight(),paint);
+        View view=(View)findViewById(R.id.main_imageView);
+        view.invalidate();
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Color color=new Color();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_rot) {
+            paint.setColor(Color.rgb(230,50,0));
+        } else if (id == R.id.nav_blau) {
+            paint.setColor(Color.rgb(30,30,200));
+        } else if (id == R.id.nav_gruen) {
+            paint.setColor(Color.rgb(30,170,40));
+        } else if (id == R.id.nav_braun) {
+            paint.setColor(Color.rgb(170,100,30));
+        } else if (id == R.id.nav_gelb) {
+            paint.setColor(Color.rgb(255,250,10));
+        } else if (id == R.id.nav_haut) {
+            paint.setColor(Color.rgb(255,230,180));
+        }else if (id == R.id.nav_schwarz) {
+            paint.setColor(Color.rgb(0,0,0));
+        }
+        else if (id == R.id.nav_kleiner) {
+            try {
+                BrushSize=BrushSize-10;
+            }catch (Exception ex) {}
+        }
+        else if (id == R.id.nav_groesser) {
+            try {
+                BrushSize=BrushSize+10;
+            }catch (Exception ex) {}
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void touch_move(View view,float x, float y)
+    {
+        canvas.drawCircle(x,y,BrushSize,paint);
+        view.invalidate();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        float x = motionEvent.getX();
+        float y = motionEvent.getY();
+
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touch_move(view,x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touch_move(view,x, y);
+                break;
+        }
         return true;
     }
 }
